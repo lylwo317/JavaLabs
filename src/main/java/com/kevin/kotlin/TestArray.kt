1520fb997b53d39730b0c2356e95f60060b29148
KevinXie <lylwo317@gmail.com>
2023-02-10 09:54:29 +0800

    添加 TestArray

---
+++ src/main/java/com/kevin/kotlin/TestArray.kt
@@ -0,0 +1,23 @@
+package com.kevin.kotlin
+
+/**
+ * Created by: kevin
+ * Date: 2023-02-10
+ */
+
+fun testArray(){
+    val array = arrayOf(1L)
+    var get = array[0]
+    var iterator = array.iterator()
+}
+
+fun testLongArray(){
+    val longArray = longArrayOf(2L)
+    var l = longArray[0]
+    var iterator = longArray.iterator()
+}
+
+fun main() {
+   testArray()
+   testLongArray()
+}
